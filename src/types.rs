17da25d5179ebdcc0d13f87e62c918a11f818c1e
Chris Moreton <chris@netsensia.com>
2021-12-17 20:47:50 +0000

    I don't know Rust

--- src/types.rs
+++ src/types.rs
@@ -1,110 +1,115 @@
-use std::collections::HashMap;
+pub mod types {
+    use std::collections::HashMap;
+    use std::collections::ConsList;
 
-type Square = u64;
-type Bitboard = u64;
-type Move = u64;
-type MoveList = ConsList<Move>;
-type Path = ConsList<Move>;
-type MagicFunc = fn(Square, Int) -> Bitboard;
+    type Square = u64;
+    type Bitboard = u64;
+    type Move = u64;
+    type MoveList = ConsList<Move>;
+    type Path = ConsList<Move>;
+    type MagicFunc = fn(Square, Int) -> Bitboard;
 
-enum Mover { White, Black }
-enum Piece { Pawn, King, Queen, Bishop, Knight,  Rook }
-enum Bound { Exact, Lower, Upper }
+    enum Mover { White, Black }
 
-struct HashEntry {
-    score: u16,
-    he_path: Path,
-    height: u16,
-    bound: Bound,
-    lock: u64
-}
+    enum Piece { Pawn, King, Queen, Bishop, Knight, Rook }
 
-struct MoveScore {
-    ms_score: u16,
-    ms_bound: Bound,
-    ms_path: Path,
-}
+    enum Bound { Exact, Lower, Upper }
 
-type HashTable = HashMap<u64, HashEntry>;
-type MagicHashTable = HashMap<u64, u64>;
-
-struct Position {
-    white_pawn_bitboard: Bitboard,
-    white_knight_bitboard: Bitboard,
-    white_bishop_bitboard: Bitboard,
-    white_queen_bitboard: Bitboard,
-    white_king_bitboard: Bitboard,
-    white_rook_bitboard: Bitboard,
-    black_pawn_bitboard: Bitboard,
-    black_knight_bitboard: Bitboard,
-    black_bishop_bitboard: Bitboard,
-    black_queen_bitboard: Bitboard,
-    black_king_bitboard: Bitboard,
-    black_rook_bitboard: Bitboard,
-    all_pieces_bitboard: Bitboard,
-    white_pieces_bitboard: Bitboard,
-    black_pieces_bitboard: Bitboard,
-    mover: Mover,
-    en_passant_square: Square,
-    white_king_castle_available: Bool,
-    black_king_castle_available: Bool,
-    white_queen_castle_available: Bool,
-    black_queen_castle_available: Bool,
-    half_moves: Int,
-    move_number: Int,
-}
-
-impl PartialEq for Position {
-    fn eq(&self, other: &Self) -> bool {
-        self.whitePawnBitboard == other.whitePawnBitboard &&
-        self.whiteKnightBitboard == other.whiteKnightBitboard &&
-        self.whiteBishopBitboard == other.whiteBishopBitboard &&
-        self.whiteQueenBitboard == other.whiteQueenBitboard &&
-        self.whiteKingBitboard == other.whiteKingBitboard &&
-        self.whiteRookBitboard == other.whiteRookBitboard &&
-        self.blackPawnBitboard == other.blackPawnBitboard &&
-        self.blackKnightBitboard == other.blackKnightBitboard &&
-        self.blackBishopBitboard == other.blackBishopBitboard &&
-        self.blackQueenBitboard == other.blackQueenBitboard &&
-        self.blackKingBitboard == other.blackKingBitboard &&
-        self.blackPiecesBitboard == other.blackPiecesBitboard &&
-        self.enPassantSquare == other.enPassantSquare &&
-        self.whiteKingCastleAvailable == other.whiteKingCastleAvailable &&
-        self.whiteQueenCastleAvailable == other.whiteQueenCastleAvailable &&
-        self.blackKingCastleAvailable == other.blackKingCastleAvailable &&
-        self.blackQueenCastleAvailable == other.blackQueenCastleAvailable &&
-        self.mover == other.mover
+    struct HashEntry {
+        score: u16,
+        he_path: Path,
+        height: u16,
+        bound: Bound,
+        lock: u64
     }
-}
 
-impl Eq for Position { }
-
-fn bitboard_for_mover(&position: Position, &piece: Piece) {
-    bitboardForColour(position, position.mover, piece)
-}
-
-fn bitboard_for_colour(&position: Position, &mover: Mover, &piece: Piece) {
-    match (mover, piece) {
-        (Mover::White, Piece::King) => position.white_king_bitboard,
-        (Mover::White, Piece::Queen) => position.white_queen_bitboard,
-        (Mover::White, Piece::Rook) => position.white_rook_bitboard,
-        (Mover::White, Piece::Knight) => position.white_knight_bitboard,
-        (Mover::White, Piece::Bishop) => position.white_bishop_bitboard,
-        (Mover::White, Piece::Pawn) => position.white_pawn_bitboard,
-        (Mover::Black, Piece::King) => position.black_king_bitboard,
-        (Mover::Black, Piece::Queen) => position.black_queen_bitboard,
-        (Mover::Black, Piece::Rook) => position.black_rook_bitboard,
-        (Mover::Black, Piece::Knight) => position.black_knight_bitboard,
-        (Mover::Black, Piece::Bishop) => position.black_bishop_bitboard,
-        (Mover::Black, Piece::Pawn) => position.black_pawn_bitboard,
+    struct MoveScore {
+        ms_score: u16,
+        ms_bound: Bound,
+        ms_path: Path,
     }
-}
 
-fn slider_bitboard_for_colour(&position: Position, &mover: Mover, &piece: Piece) {
-    match (mover, piece) {
-        (Mover::White, Piece::Rook) => position.white_rook_bitboard | position.white_queen_bitboard,
-        (Mover::White, Piece::Bishop) => position.white_bishop_bitboard | position.white_queen_bitboard,
-        (Mover::Black, Piece::Rook) => position.black_rook_bitboard | position.black_queen_bitboard,
-        (Mover::Black, Piece::Bishop) => position.black_bishop_bitboard | position.black_queen_bitboard,
+    type HashTable = HashMap<u64, HashEntry>;
+    type MagicHashTable = HashMap<u64, u64>;
+
+    struct Position {
+        white_pawn_bitboard: Bitboard,
+        white_knight_bitboard: Bitboard,
+        white_bishop_bitboard: Bitboard,
+        white_queen_bitboard: Bitboard,
+        white_king_bitboard: Bitboard,
+        white_rook_bitboard: Bitboard,
+        black_pawn_bitboard: Bitboard,
+        black_knight_bitboard: Bitboard,
+        black_bishop_bitboard: Bitboard,
+        black_queen_bitboard: Bitboard,
+        black_king_bitboard: Bitboard,
+        black_rook_bitboard: Bitboard,
+        all_pieces_bitboard: Bitboard,
+        white_pieces_bitboard: Bitboard,
+        black_pieces_bitboard: Bitboard,
+        mover: Mover,
+        en_passant_square: Square,
+        white_king_castle_available: Bool,
+        black_king_castle_available: Bool,
+        white_queen_castle_available: Bool,
+        black_queen_castle_available: Bool,
+        half_moves: Int,
+        move_number: Int,
+    }
+
+    impl PartialEq for Position {
+        fn eq(&self, other: &Self) -> bool {
+            self.whitePawnBitboard == other.whitePawnBitboard &&
+                self.whiteKnightBitboard == other.whiteKnightBitboard &&
+                self.whiteBishopBitboard == other.whiteBishopBitboard &&
+                self.whiteQueenBitboard == other.whiteQueenBitboard &&
+                self.whiteKingBitboard == other.whiteKingBitboard &&
+                self.whiteRookBitboard == other.whiteRookBitboard &&
+                self.blackPawnBitboard == other.blackPawnBitboard &&
+                self.blackKnightBitboard == other.blackKnightBitboard &&
+                self.blackBishopBitboard == other.blackBishopBitboard &&
+                self.blackQueenBitboard == other.blackQueenBitboard &&
+                self.blackKingBitboard == other.blackKingBitboard &&
+                self.blackPiecesBitboard == other.blackPiecesBitboard &&
+                self.enPassantSquare == other.enPassantSquare &&
+                self.whiteKingCastleAvailable == other.whiteKingCastleAvailable &&
+                self.whiteQueenCastleAvailable == other.whiteQueenCastleAvailable &&
+                self.blackKingCastleAvailable == other.blackKingCastleAvailable &&
+                self.blackQueenCastleAvailable == other.blackQueenCastleAvailable &&
+                self.mover == other.mover
+        }
+    }
+
+    impl Eq for Position {}
+
+    fn bitboard_for_mover(&position: Position, &piece: Piece) -> Bitboard {
+        bitboardForColour(position, position.mover, piece)
+    }
+
+    fn bitboard_for_colour(&position: Position, &mover: Mover, &piece: Piece) {
+        match (mover, piece) {
+            (Mover::White, Piece::King) => position.white_king_bitboard,
+            (Mover::White, Piece::Queen) => position.white_queen_bitboard,
+            (Mover::White, Piece::Rook) => position.white_rook_bitboard,
+            (Mover::White, Piece::Knight) => position.white_knight_bitboard,
+            (Mover::White, Piece::Bishop) => position.white_bishop_bitboard,
+            (Mover::White, Piece::Pawn) => position.white_pawn_bitboard,
+            (Mover::Black, Piece::King) => position.black_king_bitboard,
+            (Mover::Black, Piece::Queen) => position.black_queen_bitboard,
+            (Mover::Black, Piece::Rook) => position.black_rook_bitboard,
+            (Mover::Black, Piece::Knight) => position.black_knight_bitboard,
+            (Mover::Black, Piece::Bishop) => position.black_bishop_bitboard,
+            (Mover::Black, Piece::Pawn) => position.black_pawn_bitboard,
+        }
+    }
+
+    fn slider_bitboard_for_colour(&position: Position, &mover: Mover, &piece: Piece) {
+        match (mover, piece) {
+            (Mover::White, Piece::Rook) => position.white_rook_bitboard | position.white_queen_bitboard,
+            (Mover::White, Piece::Bishop) => position.white_bishop_bitboard | position.white_queen_bitboard,
+            (Mover::Black, Piece::Rook) => position.black_rook_bitboard | position.black_queen_bitboard,
+            (Mover::Black, Piece::Bishop) => position.black_bishop_bitboard | position.black_queen_bitboard,
+        }
     }
 }
---
+++ tests/types.rs
@@ -0,0 +1,70 @@
+use types;
+
+#[test]
+fn it_returns_the_correct_bitboard_for_mover() {
+    let p1 = Position {
+        white_pawn_bitboard: 1,
+        white_knight_bitboard: 2,
+        white_bishop_bitboard: 3,
+        white_queen_bitboard: 4,
+        white_king_bitboard: 5,
+        white_rook_bitboard: 6,
+        black_pawn_bitboard: 7,
+        black_knight_bitboard: 8,
+        black_bishop_bitboard: 9,
+        black_queen_bitboard: 10,
+        black_king_bitboard: 11,
+        black_rook_bitboard: 12,
+        all_pieces_bitboard: 13,
+        white_pieces_bitboard: 14,
+        black_pieces_bitboard: 15,
+        mover: Mover::White,
+        en_passant_square: 1,
+        white_king_castle_available: true,
+        black_king_castle_available: true,
+        white_queen_castle_available: true,
+        black_queen_castle_available: true,
+        half_moves: 0,
+        move_number: 0,
+    };
+
+    assert_eq!(1, bitboard_for_mover(&p1, Piece::Pawn));
+    assert_eq!(2, bitboard_for_mover(&p1, Piece::Knight));
+    assert_eq!(3, bitboard_for_mover(&p1, Piece::Bishop));
+    assert_eq!(4, bitboard_for_mover(&p1, Piece::Queen));
+    assert_eq!(5, bitboard_for_mover(&p1, Piece::King));
+    assert_eq!(6, bitboard_for_mover(&p1, Piece::Rook));
+
+    let p2 = Position {
+        white_pawn_bitboard: 1,
+        white_knight_bitboard: 2,
+        white_bishop_bitboard: 3,
+        white_queen_bitboard: 4,
+        white_king_bitboard: 5,
+        white_rook_bitboard: 6,
+        black_pawn_bitboard: 7,
+        black_knight_bitboard: 8,
+        black_bishop_bitboard: 9,
+        black_queen_bitboard: 10,
+        black_king_bitboard: 11,
+        black_rook_bitboard: 12,
+        all_pieces_bitboard: 13,
+        white_pieces_bitboard: 14,
+        black_pieces_bitboard: 15,
+        mover: Mover::Black,
+        en_passant_square: 1,
+        white_king_castle_available: true,
+        black_king_castle_available: true,
+        white_queen_castle_available: true,
+        black_queen_castle_available: true,
+        half_moves: 0,
+        move_number: 0,
+    };
+
+    assert_eq!(7, bitboard_for_mover(&p2, Piece::Pawn));
+    assert_eq!(8, bitboard_for_mover(&p2, Piece::Knight));
+    assert_eq!(9, bitboard_for_mover(&p2, Piece::Bishop));
+    assert_eq!(10, bitboard_for_mover(&p2, Piece::Queen));
+    assert_eq!(11, bitboard_for_mover(&p2, Piece::King));
+    assert_eq!(12, bitboard_for_mover(&p2, Piece::Rook));
+}
