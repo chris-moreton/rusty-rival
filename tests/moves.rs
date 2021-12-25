5ae0ebc34c7cecc4822f39cef205787ba5119567
Chris Moreton <chris@netsensia.com>
2021-12-25 20:54:56 +0000

    it_gets_all_pieces_bitboard

--- tests/moves.rs
+++ tests/moves.rs
@@ -6,3 +6,9 @@ fn it_gets_all_bits_except_friendly_pieces() {
     let position = get_position(&"n5k1/6n1/1n2q2p/1p5P/1P3RP1/2PK1B2/1r2N3/8 b kQKq g3 5 56".to_string());
     assert_eq!(all_bits_except_friendly_pieces(&position), 0b0111110111111101101101101011111111111111111111111011111111111111);
 }
+
+#[test]
+fn it_gets_all_pieces_bitboard() {
+    let position = get_position(&"n5k1/6n1/1n2q2p/1p5P/1P3RP1/2PK1B2/1r2N3/8 b kQKq g3 5 56".to_string());
+    assert_eq!(position.all_pieces_bitboard, 0b1000001000000010010010010100000101000110001101000100100000000000);
+}
