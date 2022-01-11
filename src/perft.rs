934ac7a2c60df87fde355d2f11801575312753b0
Chris Moreton <chris@netsensia.com>
2022-01-11 20:27:12 +0000

    Simplify Perft tests

--- src/perft.rs
+++ src/perft.rs
@@ -1,23 +1,29 @@
-use crate::make_move::{make_move, switch_side, unmake_move};
-use crate::moves::{is_check, moves};
+use crate::make_move::{default_position_history, make_move, switch_side, unmake_move};
+use crate::moves::{allocate_magic_boxes, is_check, moves};
 use crate::types::{MagicBox, Position, PositionHistory};
 
-pub fn perft(position: &mut Position, depth: u8, history: &mut PositionHistory, magic_box: &MagicBox) -> u64 {
+pub fn perft(position: &mut Position, depth: u8) -> u64 {
 
-    let mut count = 0;
+    pub fn perft(position: &mut Position, depth: u8, history: &mut PositionHistory, magic_box: &MagicBox) -> u64 {
+        let mut count = 0;
 
-    moves(position, magic_box).into_iter().for_each(|m| {
-        make_move(position, m, history);
-        if !is_check(position, switch_side(position.mover), magic_box) {
-            count += if depth == 0 {
-                1
-            } else {
-                perft(position, depth-1, history, magic_box)
+        moves(position, magic_box).into_iter().for_each(|m| {
+            make_move(position, m, history);
+            if !is_check(position, switch_side(position.mover), magic_box) {
+                count += if depth == 0 {
+                    1
+                } else {
+                    perft(position, depth - 1, history, magic_box)
+                }
             }
-        }
-        unmake_move(position, history)
-    });
+            unmake_move(position, history)
+        });
 
-    count
+        count
+    }
 
+    let mut history = default_position_history();
+    let magic_box = allocate_magic_boxes();
+
+    perft(position, depth, &mut history, &magic_box)
 }
