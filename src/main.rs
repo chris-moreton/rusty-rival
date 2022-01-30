78a81ab7d826e93aa3773d4a5abf1b1cdf6c9667
Chris Moreton <chris@netsensia.com>
2022-01-30 22:50:52 +0000

    Floating point learning

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ fn main() {
         let nodes = perft(&mut get_position(&"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1".to_string()), 4);
         let duration = start.elapsed();
         println!("Time elapsed in perft is: {:?}", duration);
-        println!("{} nodes {} nps", nodes, nodes.div(duration.as_secs()));
+        println!("{} nodes {} nps", nodes, (nodes as f64 / (duration.as_millis() as f64)) * 1000.0);
     }
 }
