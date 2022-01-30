12d78d4998a8c42c7b5f3588d14aa9360b4a07ff
Chris Moreton <chris@netsensia.com>
2022-01-30 16:44:50 +0000

    Unchecked lookups

--- src/perft.rs
+++ src/perft.rs
@@ -4,25 +4,20 @@ use crate::moves::{is_check, moves};
 use crate::types::{Position};
 
 pub unsafe fn perft(position: &Position, depth: u8) -> u64 {
+    let mut count = 0;
+    let mover = position.mover;
 
-    pub unsafe fn perft(position: &Position, depth: u8) -> u64 {
-        let mut count = 0;
-        let mover = position.mover;
-
-        for m in moves(position) {
-            let mut new_position = mem::MaybeUninit::<Position>::uninit();
-            make_move(position, m, &mut *new_position.as_mut_ptr());
-            if !is_check(&*new_position.as_ptr(), mover) {
-                count += if depth == 0 {
-                    1
-                } else {
-                    perft(&*new_position.as_ptr(), depth - 1)
-                }
+    for m in moves(position) {
+        let mut new_position = mem::MaybeUninit::<Position>::uninit();
+        make_move(position, m, &mut *new_position.as_mut_ptr());
+        if !is_check(&*new_position.as_ptr(), mover) {
+            count += if depth == 0 {
+                1
+            } else {
+                perft(&*new_position.as_ptr(), depth - 1)
             }
-        };
+        }
+    };
 
-        count
-    }
-
-    perft(position, depth)
+    count
 }
