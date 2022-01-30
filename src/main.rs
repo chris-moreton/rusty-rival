a8ebfdcfb99ceaaa673d740f9391b0ed50f4b945
Chris Moreton <chris@netsensia.com>
2022-01-30 18:14:00 +0000

    Magic tuples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::ops::Div;
 use std::time::Instant;
 use rusty_rival::fen::get_position;
 use rusty_rival::perft::perft;
@@ -5,8 +6,9 @@ use rusty_rival::perft::perft;
 fn main() {
     let start = Instant::now();
     unsafe {
-        println!("{}", perft(&mut get_position(&"r3k2r/p6p/8/B7/1pp1p3/3b4/P6P/R3K2R w KQkq - 0 1".to_string()), 5));
+        let nodes = perft(&mut get_position(&"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1".to_string()), 4);
+        let duration = start.elapsed();
+        println!("Time elapsed in perft is: {:?}", duration);
+        println!("{} nodes {} nps", nodes, nodes.div(duration.as_secs()));
     }
-    let duration = start.elapsed();
-    println!("Time elapsed in perft is: {:?}", duration);
 }
