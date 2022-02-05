0973b8f458ef37d3d901a81d388b6e7df00e221a
Chris Moreton <chris@netsensia.com>
2022-02-05 15:39:15 +0000

    Add bestmove test

--- tests/uci.rs
+++ tests/uci.rs
@@ -3,7 +3,7 @@ use rusty_rival::uci::run_command;
 
 #[test]
 pub fn it_sets_a_fen() {
-    let mut fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    let mut fen = "".to_string();
     assert_eq!(run_command(&mut fen, "position fen rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1"), Right(None));
     assert_eq!(*fen.to_string(), "rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1".to_string());
     assert_eq!(run_command(&mut fen, "go perft 1"), Right(None))
@@ -11,15 +11,31 @@ pub fn it_sets_a_fen() {
 
 #[test]
 pub fn it_runs_a_perft_test() {
-    let mut fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    let mut fen = "".to_string();
     assert_eq!(run_command(&mut fen, "position fen rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1"), Right(None));
-    assert_eq!(*fen.to_string(), "rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1".to_string());
     assert_eq!(run_command(&mut fen, "go perft 2"), Right(None))
 }
 
 #[test]
 pub fn it_handles_a_bad_fen() {
-    let mut fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string();
+    let mut fen = "".to_string();
     let command = "position fen rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0";
     assert_eq!(run_command(&mut fen, command), Left("Invalid FEN".to_string()));
+}
+
+#[test]
+pub fn it_returns_a_best_move() {
+    let mut fen = "".to_string();
+    assert_eq!(run_command(&mut fen, "position fen rnbqkbnr/pppppppp/8/8/PPPPPPPP/8/8/RNBQKBNR w KQkq - 0 1"), Right(None));
+    let result = run_command(&mut fen, "go depth 3");
+    match result {
+        Left(error) => panic!("Fail"),
+        Right(Some(message)) => {
+            assert!(message.contains("bestmove"))
+        },
+        _ => {
+            panic!("Fail")
+        }
+
+    }
 }
