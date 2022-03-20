ca5cef260dbf32a98872f43806496e29eeaefc0c
Chris Moreton <chris@netsensia.com>
2022-03-20 01:00:01 +0000

    Tagged 20220320-01-Hash-128

--- src/engine_constants.rs
+++ src/engine_constants.rs
@@ -20,7 +20,7 @@ pub const MAX_QUIESCE_DEPTH: u8 = 10;
 pub const IID_REDUCE_DEPTH: u8 = 3;
 
 pub const HASH_ENTRY_BYTES: u64 = 22;
-pub const HASH_ENTRY_MB: u64 = 32;
+pub const HASH_ENTRY_MB: u64 = 128;
 pub const NUM_HASH_ENTRIES: u64 = ((1024 * 1024 * HASH_ENTRY_MB) / HASH_ENTRY_BYTES) as u64;
 
 pub const TICKER_MILLIS: u16 = 500;
