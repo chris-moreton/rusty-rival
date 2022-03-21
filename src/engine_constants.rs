844471d72b8e6df4d4d1367a9774f5b45c4ad679
Chris Moreton <chris@netsensia.com>
2022-03-21 14:27:54 +0000

    Tagged 20220321-02-Rebuild-1

--- src/engine_constants.rs
+++ src/engine_constants.rs
@@ -8,7 +8,7 @@ pub const BISHOP_VALUE: Score = 350;
 pub const ROOK_VALUE: Score = 550;
 pub const QUEEN_VALUE: Score = 900;
 
-pub const UCI_MILLIS_REDUCTION: u128 = 75;
+pub const UCI_MILLIS_REDUCTION: u128 = 5;
 
 pub const NUM_KILLER_MOVES: usize = 2;
 
@@ -18,7 +18,10 @@ pub const MAX_DEPTH: u8 = 250;
 
 pub const MAX_QUIESCE_DEPTH: u8 = 10;
 pub const IID_REDUCE_DEPTH: u8 = 3;
-pub const NUM_HASH_ENTRIES: u64 = 1024 * 1024 * 4;
+
+pub const HASH_ENTRY_BYTES: u64 = 22;
+pub const HASH_ENTRY_MB: u64 = 128;
+pub const NUM_HASH_ENTRIES: u64 = ((1024 * 1024 * HASH_ENTRY_MB) / HASH_ENTRY_BYTES) as u64;
 
 pub const TICKER_MILLIS: u16 = 500;
 
