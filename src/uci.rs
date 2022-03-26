92a44fb55032dfe77e85ebb6117677fcdf025a88
Chris Moreton <chris@netsensia.com>
2022-03-26 15:48:51 +0000

    Tagged 20220326-05-More-Non-Functional-Updates

--- src/uci.rs
+++ src/uci.rs
@@ -259,7 +259,7 @@ fn calc_from_colour_times(mut uci_state: &mut UciState, millis: u64, inc_millis:
 }
 
 fn cmd_uci() -> Either<String, Option<String>> {
-    Right(Some("id name Rusty Rival |20220326-02-Aspiration-Refactor-2|\nid author Chris Moreton\noption name Clear Hash type button\nuciok".parse().unwrap()))
+    Right(Some("id name Rusty Rival |20220326-05-More-Non-Functional-Updates|\nid author Chris Moreton\noption name Clear Hash type button\nuciok".parse().unwrap()))
 }
 
 fn cmd_isready() -> Either<String, Option<String>> {
