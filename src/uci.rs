a4d3bf614aa1b80307e5d00fd92b15d0e97215b4
Chris Moreton <chris@netsensia.com>
2022-04-22 19:31:35 +0100

    Tagged 20220422-02-Returning-SEE

--- src/uci.rs
+++ src/uci.rs
@@ -301,7 +301,7 @@ fn calc_from_colour_times(mut uci_state: &mut UciState, millis: u64, inc_millis:
 
 fn cmd_uci() -> Either<String, Option<String>> {
     Right(Some(
-        r#"id name Rusty Rival |20220422-01-Internal-Draw-Detection|
+        r#"id name Rusty Rival |20220422-02-Returning-SEE|
 id author Chris Moreton
 option name Clear Hash type button
 option name MultiPV type spin default 1 min 1 max 20
