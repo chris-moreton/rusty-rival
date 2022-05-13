23ddcab34e2950b769ad69b7a94e60c40c4bc58b
Chris Moreton <chris@netsensia.com>
2022-05-13 11:18:47 +0100

    Tagged 20220513-01-Twelves

--- src/uci.rs
+++ src/uci.rs
@@ -301,7 +301,7 @@ fn calc_from_colour_times(mut uci_state: &mut UciState, millis: u64, inc_millis:
 
 fn cmd_uci() -> Either<String, Option<String>> {
     Right(Some(
-        r#"id name Rusty Rival |20220509-01-Doubled-Bishop-Threats|
+        r#"id name Rusty Rival |20220513-01-Twelves|
 id author Chris Moreton
 option name Clear Hash type button
 option name MultiPV type spin default 1 min 1 max 20
