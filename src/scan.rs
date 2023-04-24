f56405e0f395fc249d281cce0495b0663fdb22ad
Chris Moreton <chris@netsensia.com>
2023-04-24 17:49:33 +0100

    Try without saving

--- src/scan.rs
+++ src/scan.rs
@@ -4,7 +4,7 @@ use opencv::prelude::*;
 use opencv::{core::{Mat, Size}, highgui, imgcodecs, imgproc::*, videoio, Result, imgproc, ximgproc};
 use std::time::{Duration, Instant};
 use opencv::calib3d::{CALIB_CB_ADAPTIVE_THRESH, CALIB_CB_NORMALIZE_IMAGE, draw_chessboard_corners, find_chessboard_corners};
-use opencv::core::{Point, Point2f, Rect, Vector, ToInputArray, InputArray, count_non_zero, BORDER_DEFAULT, Size_, min_max_loc, subtract, no_array, pow, sum_elems, absdiff, norm};
+use opencv::core::{Point, Point2f, Rect, Vector, ToInputArray, InputArray, count_non_zero, BORDER_DEFAULT, Size_, min_max_loc, subtract, no_array, pow, sum_elems, absdiff, norm, CV_8UC4};
 use screenshots::Screen;
 use std::{fs};
 use std::io::Read;
@@ -19,6 +19,12 @@ use crate::moves::is_check;
 use crate::search::iterative_deepening;
 use crate::types::{BLACK, default_search_state, WHITE};
 use crate::utils::invert_fen;
+use scrap::{Capturer, Display};
+use image::{ImageBuffer, Rgba};
+use std::error::Error;
+use std::ffi::c_void;
+use std::thread;
+use std::fmt;
 
 const RESIZED_BOARD_IMAGE_WIDTH: i32 = 1024;
 
@@ -69,9 +75,17 @@ pub fn screen_scan(flipped_board: bool) -> Result<()> {
         for (i, square) in squares.iter().enumerate() {
             let center = extract_center(&square)?;
             let piece = match_piece_image(&center)?;
-            let square_num = i + 1;
 
             if let Some(first_char) = piece.chars().next() {
+                if first_char == 'K' && piece_list.contains(&'K') {
+                    println!("White has more than one king");
+                    exit(1);
+                }
+                if first_char == 'k' && piece_list.contains(&'k') {
+                    println!("Black has more than one king");
+                    exit(1);
+                }
+
                 piece_list.push(first_char);
             } else {
                 println!("The input string is empty.");
@@ -79,6 +93,7 @@ pub fn screen_scan(flipped_board: bool) -> Result<()> {
         }
 
         if !piece_list.is_empty() {
+
             let mut castle_string = "-".to_string();
             let mut fen = vec_to_fen(&piece_list, "w", &castle_string);
 
@@ -118,11 +133,10 @@ pub fn screen_scan(flipped_board: bool) -> Result<()> {
         while !is_screenshot_ready(chessboard_x, chessboard_y, chessboard_width)? { }
 
     }
-
-    Ok(())
 }
 
 fn is_screenshot_ready(chessboard_x: i32, chessboard_y: i32, chessboard_width: i32) -> Result<bool> {
+
     Command::new("screencapture")
         .args(&["-D1", "-x", "-t", "png", "/tmp/screenshot.png"])
         .output().map_err(|e| e.to_string()).expect("TODO: panic message");
@@ -136,6 +150,10 @@ fn is_screenshot_ready(chessboard_x: i32, chessboard_y: i32, chessboard_width: i
     let s = imgcodecs::imread("/tmp/screenshot.png", imgcodecs::IMREAD_COLOR)?;
     let s2 = imgcodecs::imread("/tmp/screenshot2.png", imgcodecs::IMREAD_COLOR)?;
 
+    // let s = capture()?;
+    // sleep(Duration::from_millis(50));
+    // let s2 = capture()?;
+
     let roi = Rect::new(chessboard_x, chessboard_y, chessboard_width, chessboard_width);
 
     let mut c = Mat::roi(&s, roi)?;
@@ -146,6 +164,77 @@ fn is_screenshot_ready(chessboard_x: i32, chessboard_y: i32, chessboard_width: i
 
     return Ok(compare_files("/tmp/board.png", "/tmp/board2.png"))
 
+}
+
+fn capture() -> Result<Mat> {
+    let display = Display::primary();
+
+    match display {
+        Ok(display) => {
+            let mut capturer = Capturer::new(display);
+
+            match capturer {
+                Ok(mut capturer) => {
+                    let (width, height) = (capturer.width(), capturer.height());
+                    let frame = loop {
+                        match capturer.frame() {
+                            Ok(frame) => break frame,
+                            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                                thread::sleep(Duration::from_millis(100));
+                                continue;
+                            },
+                            Err(e) => {
+                                println!("Failed to capture frame: {}", e);
+                                exit(1)
+                            },
+                        }
+                    };
+
+                    let buffer =
+                        ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width as u32, height as u32, frame.to_vec())
+                            .ok_or("Failed to create image buffer");
+
+                    match buffer {
+                        Ok(buffer) => {
+                            let mat = imgbuf_to_mat(&buffer);
+                            match mat {
+                                Ok(mat) => Ok(mat),
+                                Err(e) => {
+                                    println!("Failed to create mat: {}", e);
+                                    exit(1)
+                                }
+                            }
+                        },
+                        Err(e) => {
+                            println!("Failed to create image buffer: {}", e);
+                            exit(1)
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to create capturer: {}", e);
+                    exit(1)
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to get display");
+            exit(1)
+        }
+    }
+
+}
+
+fn imgbuf_to_mat(imgbuf: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> Result<Mat, Box<dyn Error>> {
+    let (width, height) = (imgbuf.width(), imgbuf.height());
+    let data = imgbuf.as_flat_samples();
+    let step = 4 * width;
+
+    let mat: Mat = unsafe {
+        Mat::new_rows_cols_with_data(height as i32, width as i32, CV_8UC4, data.as_slice().as_ptr() as *mut c_void, step as usize)?
+    };
+
+    Ok(mat)
 }
 
 fn compare_files(path1: &str, path2: &str) -> bool {
@@ -289,14 +378,6 @@ fn match_piece_image(image: &Mat) -> Result<String> {
         }
     };
 
-    println!(
-        "{} {} {:.4} {}",
-        white_pixels,
-        black_pixels,
-        ratio,
-        answer
-    );
-
     Ok(answer.to_string())
 }
 
@@ -304,7 +385,6 @@ fn is_knight(square: &Mat) -> Result<bool> {
     let image = extract_top_quarter(&square)?;
 
     let white_pixels = count_pixels(&image, PixelColor::White)?;
-    let black_pixels = count_pixels(&image, PixelColor::Black)?;
 
     return Ok(white_pixels > 0)
 }
