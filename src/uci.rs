69a4adccb3ac06cd2005f5144fef28d9c558984e
Chris Moreton <chris@netsensia.com>
2023-04-27 18:56:59 +0100

    Tagged 20230427.0

--- src/uci.rs
+++ src/uci.rs
@@ -307,7 +307,7 @@ fn calc_from_colour_times(mut uci_state: &mut UciState, millis: u64, inc_millis:
 
 fn cmd_uci() -> Either<String, Option<String>> {
     Right(Some(
-        r#"id name Rusty Rival |20230427.1|
+        r#"id name Rusty Rival |20230427.0|
 id author Chris Moreton
 option name Clear Hash type button
 option name MultiPV type spin default 1 min 1 max 20
